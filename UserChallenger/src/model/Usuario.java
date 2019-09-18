a8d80aa8e68d2960e6ca0e79715e0077133c56c3
João <jcss.silva@hotmail.com>
2019-09-18 10:54:25 -0300

    novos campos

--- UserChallenger/src/model/Usuario.java
+++ UserChallenger/src/model/Usuario.java
@@ -1,5 +1,29 @@
 package model;
 
 public class Usuario {
-
+	private String nome;
+	private String email;
+	private String senha;
+	
+	//Get and Set
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
 }
